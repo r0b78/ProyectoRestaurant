764ed54ccee2be2d6f78bec935a0a47c893310ee
r0b78 <robert19972008@hotmail.com>
2016-10-12 16:06:07 -0600

    Esta no es mary ? o si ?
    Esto

--- ProyectoRestaurant.java
+++ ProyectoRestaurant.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyectorestaurant;
+
 
 /**
  *
@@ -17,6 +17,7 @@ public class ProyectoRestaurant {
     public static void main(String[] args) {
         // TODO code application logic here
 //Esto
+    	//Este es nuevo :D
     }
     
 }
